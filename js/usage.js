60ee07bcefbc4556a0661f378e288d51c6368d0c
sam-ginsburg <sbg11@duke.edu>
2014-03-06 10:53:05 -0500

    implemented file overwriting

--- js/usage.js
+++ js/usage.js
@@ -5,20 +5,43 @@ var documents = Database.load();
 var filelist = document.getElementById("availableFiles");
 for(var i = 0; i < documents.length; i++){
 	var newItem = document.createElement("li");
-	newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' onClick='getDoc(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
+	newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' id='" + myDoc.name + "' onClick='getDoc(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
 	filelist.appendChild(newItem);
 }
 
 
 function createNewDoc(){
+
+	var exists = false;
+	var index;
+	for(var i = 0; i < documents.length; i++){
+		if (documents[i].name == document.getElementById('name').value){
+			index = i;
+			exists = true;
+			console.log(documents[i].content);
+		}
+	}
+
 	myDoc.name = document.getElementById('name').value;
 	myDoc.content = document.getElementById('editor').value;
-	console.log(myDoc);
-	Database.save(myDoc);
-
 	var newItem = document.createElement("li");
-	newItem.innerHTML = myDoc.name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' onClick='getDoc(" + JSON.stringify(myDoc) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(myDoc) + ")'>X</button>";
-	filelist.appendChild(newItem);
+	newItem.innerHTML = myDoc.name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' id='" + myDoc.name + "' onClick='getDoc(" + JSON.stringify(myDoc) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(myDoc) + ")'>X</button>";
+
+	if(!exists){
+		console.log(myDoc);
+		Database.save(myDoc);
+
+		filelist.appendChild(newItem);
+	}
+
+	else{
+		Database.remove(documents[index]);
+
+		console.log(myDoc);
+		Database.save(myDoc);
+
+		filelist.appendChild(newItem);
+	}
 }
 
 function getDoc(myDoc){

7dd8d971f2b30acdfcba15909c967b21a9f162e4
sam-ginsburg <sbg11@duke.edu>
2014-03-06 09:20:22 -0500

    improved file selecting/getting/loading

--- js/usage.js
+++ js/usage.js
@@ -4,10 +4,11 @@ var documents = Database.load();
 
 var filelist = document.getElementById("availableFiles");
 for(var i = 0; i < documents.length; i++){
-		var newItem = document.createElement("li");
-		newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' data-dismiss='alert' onClick='getDoc(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
-		filelist.appendChild(newItem);
+	var newItem = document.createElement("li");
+	newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' onClick='getDoc(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
+	filelist.appendChild(newItem);
 }
+
 
 function createNewDoc(){
 	myDoc.name = document.getElementById('name').value;
@@ -16,14 +17,13 @@ function createNewDoc(){
 	Database.save(myDoc);
 
 	var newItem = document.createElement("li");
-	newItem.innerHTML = myDoc.name + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='delete(" + JSON.stringify(myDoc) + ")'>x</button>";
+	newItem.innerHTML = myDoc.name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' onClick='getDoc(" + JSON.stringify(myDoc) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(myDoc) + ")'>X</button>";
 	filelist.appendChild(newItem);
 }
 
 function getDoc(myDoc){
 	document.getElementById('editor').value = myDoc.content;
 	document.getElementById('name').value = myDoc.name;
-		var newItem = document.createElement("li");
-		newItem.innerHTML = myDoc.name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' data-dismiss='alert' onClick='getDoc(" + JSON.stringify(myDoc) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(myDoc) + ")'>X</button>";
-		filelist.appendChild(newItem);
 }
+
+//data-dismiss='alert'

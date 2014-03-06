2af93c93d8f7cda396d37019213c9f22cbcfcdaa
sam-ginsburg <sbg11@duke.edu>
2014-03-06 08:44:25 -0500

    implemented selecting/getting docs from list

--- js/usage.js
+++ js/usage.js
@@ -5,7 +5,7 @@ var documents = Database.load();
 var filelist = document.getElementById("availableFiles");
 for(var i = 0; i < documents.length; i++){
 		var newItem = document.createElement("li");
-		newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
+		newItem.innerHTML = documents[i].name + "<button style= 'padding-left:20px; height:20px;' type='button' class='close' data-dismiss='alert' onClick='getDoc(" + JSON.stringify(documents[i]) + ")'>L</button>" + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='Database.remove(" + JSON.stringify(documents[i]) + ")'>X</button>";
 		filelist.appendChild(newItem);
 }
 
@@ -18,4 +18,9 @@ function createNewDoc(){
 	var newItem = document.createElement("li");
 	newItem.innerHTML = myDoc.name + "<button style= 'height:20px' type='button' class='close' data-dismiss='alert' onClick='delete(" + JSON.stringify(myDoc) + ")'>x</button>";
 	filelist.appendChild(newItem);
+}
+
+function getDoc(myDoc){
+	document.getElementById('editor').value = myDoc.content;
+	document.getElementById('name').value = myDoc.name;
 }
